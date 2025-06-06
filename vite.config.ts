4214c81c809e8636715f6c46e5aea726342dd49c
Alexandre Petrachini <alexandrepetrachini@gmail.com>
2025-06-06 20:03:14 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -6,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Alexandre-Petrachini.github.io/' : '/',
+  // Use "/" as the base for production since this is a user site.
+  base: "/",
   server: {
     host: "::",
     port: 8080,
